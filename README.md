cec9c99f278c83f80ddcc601eff2116b88bb03d2
DY <23586296+Kompensator@users.noreply.github.com>
2018-11-11 22:26:19 -0500

    hello world

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 # asteroids
 Simulating deflection of an asteroid on a collision course with Earth using lasers and other means.
-Changes to Hackathon file:
+Changes (newest last)
 - Physics simulation is running first and it's independent to FuncAnimation
 - Useless printing is deleted
 - Structural changes in main()
 - Can now animate a slice of the simulation
 - Weird angle bug is FIXED!! (there's some hardcoding to be done on line 96)
 - the ability to skip a certain number of frames to make animation run faster without compromising simulation
-
+- True Newtonian physics is turned ON. The bug that prevented it from working at the hackathon was FIXED.
+- More planets are been simulated and animated
+- Dark background
+- Added progress bar (optimized to reduce its affect on performance)
 
 Upcoming changes:
 - Entering burn_time, laser_power and other settings at runtime
 - Accepts polar coordinates as initial data (physics will still run with pseudo integration, no plan to use Keplerian orbits to this file)
-- Better aesthetics
--- orbit_sim.py
+++ orbit_sim.py
@@ -1,7 +1,9 @@
 """This is an improvement on hackathon_stable_5.py
 The physics engine computes the location independent of update() and then the data can be selectively animted with FuncAnimation
 The angle theta problem is FIXED
+True Newtonian physics is turned ON, and the bug that causes very little acceleration is FIXED
 There's still hardcode to be done on line 96... help?
+last edited by DY on 11/11/2018
 """
 
 import numpy as np
@@ -9,16 +11,18 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import math
 import sys
+from datetime import datetime
+import time
 
 #settings
 # everything time related are in seconds
 global sim_time, animation_start, animation_end, dt
-sim_time = int(1e9)
-dt = int(7200)
-animation_start = 1e7
+sim_time = int(1e10)
+dt = int(3600)
+animation_start = 0
 animation_end = sim_time
-frame_skip = 0              # number of frames skipped per each frame
-laser_power = 1e18
+frame_skip = 5              # number of frames skipped per each frame
+laser_power = 1e17
 burn_time = 1000000
 
 class point():
@@ -27,7 +31,6 @@ class point():
     def __init__(self, x, y):
         self.x = x
         self.y = y
-
 
 class body():
     """return a body that has a location (2d vector), mass, velocity, name
@@ -48,16 +51,15 @@ def calculate_single_body_acceleration(bodies, body_index, n, laser_power, burn_
     acceleration = point(0,0)    #initializing a zero acceleration vector
     target_body = bodies[body_index]
     for index, other_body in enumerate(bodies):    #gives a list like [(1,sun),(2,earth),....]
-        if index == 0 and index != body_index:     # this hack turns off physics between all bodies except sun
+        if index != body_index:     # the hack that only runs the physics of the Sun is turned off
             r = math.sqrt((target_body.location.x - other_body.location.x)**2 + (target_body.location.y - other_body.location.y)**2)
             try:
                 temp_acc = (g_constant * other_body.mass)/r**3    #this value multiplied by the distance in 1 dimension will give the acceleration
             except ZeroDivisionError:
                 print ("ZeroDivisionError occured in computing acceleration: r = 0")
                 temp_acc = 0
-
-            acceleration.x = temp_acc*(other_body.location.x - target_body.location.x)
-            acceleration.y = temp_acc*(other_body.location.y - target_body.location.y)
+            acceleration.x += temp_acc*(other_body.location.x - target_body.location.x)         # bug is fixed. acceleration was not additive
+            acceleration.y += temp_acc*(other_body.location.y - target_body.location.y)
         else:
             pass
     if target_body.name == "asteroid":
@@ -125,8 +127,14 @@ def compute_gravity_step(bodies, n, dt, laser_power, burn_time):
     calculate_velocity(bodies, n, dt, laser_power, burn_time)
     calculate_position(bodies, dt)
 
+def progress_bar(count, total):
+    bar_length = 60
+    filled_length = int(round(bar_length*count/total))
+    percentage = round(100*(count/total))
+    bar = '='*filled_length + '-'*(bar_length-filled_length)
+    print("%s  %s%s" %(bar,percentage,'%'), end='\r')
 
-sun = {"location":point(0,0), "mass":2e30, "velocity":point(0,0)}
+sun = {"location":point(0,0), "mass":2e30, "velocity":point(0,0)}     #sun mass = 2e30
 mercury = {"location":point(0,5.7e10), "mass":3.285e23, "velocity":point(47000,0)}
 venus = {"location":point(0,1.1e11), "mass":4.8e24, "velocity":point(35000,0)}
 earth = {"location":point(-9.124e10,-7.830e10), "mass":6e24, "velocity":point(-2.629e4,2.417e4)}
@@ -141,24 +149,29 @@ asteroid = {"location":point(-7.133e10,-1.159e11),"mass":27e9,"velocity":point(-
 bodies = [
         body( location = sun["location"], mass = sun["mass"], velocity = sun["velocity"], name = "sun"),
         body( location = earth["location"], mass = earth["mass"], velocity = earth["velocity"], name = "earth"),
-        body( location = asteroid["location"], mass = asteroid["mass"], velocity = asteroid["velocity"], name = "asteroid")]
-        #body( location = mars["location"], mass = mars["mass"], velocity = mars["velocity"], name = "mars"),
-        #body( location = venus["location"], mass = venus["mass"], velocity = venus["velocity"], name = "venus"),
-        #body( location = jupiter["location"], mass = jupiter["mass"], velocity = jupiter["velocity"], name = "jupiter"),
-        #body( location = mercury["location"], mass = mercury["mass"], velocity = mercury["velocity"], name = "mercury")]
+        body( location = asteroid["location"], mass = asteroid["mass"], velocity = asteroid["velocity"], name = "asteroid"),
+        body( location = mars["location"], mass = mars["mass"], velocity = mars["velocity"], name = "mars"),
+        body( location = venus["location"], mass = venus["mass"], velocity = venus["velocity"], name = "venus"),
+        body( location = jupiter["location"], mass = jupiter["mass"], velocity = jupiter["velocity"], name = "jupiter"),
+        body( location = mercury["location"], mass = mercury["mass"], velocity = mercury["velocity"], name = "mercury")]
 
 
 fig, ax = plt.subplots()
-ax.set_xlim(-5e11, 5e11)
-ax.set_ylim(-5e11, 5e11)
+ax.set_facecolor('xkcd:black')
+ax.set_xlim(-1e12, 1e12)
+ax.set_ylim(-1e12, 1e12)
 
 global rock_plot, trace, sun_plot, earth_plot, text
 rock_plot, = plt.plot([],[],'ro',markersize=6, animated=True)
-trace, = plt.plot([],[],'bo', markersize=0.01, animated=True)
+trace, = plt.plot([],[],'bo', markersize=0.1, animated=True)
 sun_plot, = plt.plot([],[],'yo',markersize=12, animated=True)
-earth_plot, = plt.plot([],[], "go", markersize=6, animated=True)
-text = ax.text(0, 0, '', transform = ax.transAxes,fontsize = 10)
+earth_plot, = plt.plot([],[], color='#2b63d8',marker='o',markerfacecolor='#2b63d8', markersize=7, animated=True)
+mars_plot, = plt.plot([],[], color='#f58061',marker='o',markerfacecolor='#f58061', markersize=6, animated=True)
+venus_plot, = plt.plot([],[], color="#c28033", marker='o',markerfacecolor='#c28033',markersize=5, animated=True)
+mercury_plot, = plt.plot([],[], color="#d5d2d1", marker='o',markerfacecolor='#d5d2d1',markersize=4, animated=True)
+jupiter_plot, = plt.plot([],[], color='#efd9a7',marker='o',markerfacecolor='#efd9a7',markersize=11, animated=True)
 
+text = ax.text(0, 0, '', color="white", transform=ax.transAxes,fontsize=10)
 
 def update(frame):
     start_frame = animation_start//dt
@@ -177,6 +190,14 @@ def update(frame):
             sun_plot.set_data(x_data,y_data)
         elif body.name == "earth":
             earth_plot.set_data(x_data,y_data)
+        elif body.name == "mars":
+            mars_plot.set_data(x_data,y_data)
+        elif body.name == 'venus':
+            venus_plot.set_data(x_data,y_data)
+        elif body.name == 'jupiter':
+            jupiter_plot.set_data(x_data,y_data)
+        elif body.name == 'mercury':
+            mercury_plot.set_data(x_data,y_data)
 
     trace_x, trace_y = [],[]
     for body in bodies:
@@ -184,12 +205,20 @@ def update(frame):
         trace_y.append(body.y_hist[0:frame])
     trace.set_data(trace_x,trace_y)
     text.set_text(str("Time elapsed: "+ str(frame*4)+" hours"))
-    return rock_plot, sun_plot, trace, earth_plot, text
+    return rock_plot, sun_plot, trace, earth_plot, mars_plot, venus_plot, jupiter_plot,mercury_plot, text
 
 def main(laser_power=1e18, burn_time=100000000):
+    sim_start = datetime.now()
+    start_second = time.time()
+    print ("Physics simulation started at %s" %(sim_start))
     frames = int(sim_time//dt)
     for t in range(0,frames):
         compute_gravity_step(bodies, t, dt, laser_power, burn_time)
+        if t%(int(frames/100)) == 0:        # this hack makes the whole program run 45% faster!!
+            progress_bar(t,frames)
+    sim_end = datetime.now()
+    end_second = time.time()
+    print ("Physics simulation ended at %s, time elapsed = %s seconds" %(sim_end,round(end_second-start_second)))
 
     ani = FuncAnimation(fig, update, interval=1, blit=True)
     plt.show()
